491561c97e73cf1ffbca97bc6eae32a146ddd539
Jonathan Ortuste <54188280+joni1986@users.noreply.github.com>
2022-06-03 11:06:39 -0300

    Update Gobierno.java

--- src/gobierno/Gobierno.java
+++ src/gobierno/Gobierno.java
@@ -7,7 +7,7 @@ public class Gobierno {
   //kkkk
     public static void main(String[] args) {
         System.out.println(" hola mundo"); 
-        System.out.println(" michael"); 
+        System.out.println(" casa una caraga"); 
     }
     
 }
